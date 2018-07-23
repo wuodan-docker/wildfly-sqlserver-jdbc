e14cabab41c5ab68daa488a3d1c68540140312a6
Stefan Kuhn <stefan.kuhn@energie-pool.ch>
2018-07-23 23:14:00 +0200

    Fix sql server check in start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "x${SQL_SERVER}" -ne '' ] && [ "x${SQL_SERVER_PORT}" -ne '' ]; then
+if [ -n "${SQL_SERVER}" ] && [ -n "${SQL_SERVER_PORT}" ]; then
 	until timeout 1 bash -c "cat < /dev/null > /dev/tcp/${SQL_SERVER}/${SQL_SERVER_PORT}"; do
 		echo "Wait for SQL server to start at ${SQL_SERVER}:${SQL_SERVER_PORT} ..."
 		sleep 1
